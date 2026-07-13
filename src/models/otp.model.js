b5c0d3b900ffa65a3aa3cd5e378e08b7eff5a410
Asjad <ammarzain8834@gmail.com>
2026-07-13 16:16:54 +0530

    Added the missing functionalities for the OTP verification process

--- src/models/otp.model.js
+++ src/models/otp.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const otpSchema = new monggose.Schema({
+const otpSchema = new mongoose.Schema({
   email:{
     type: String,
     required: [true, "Email is required"]
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -35,4 +35,7 @@ authRouter.get("/logout", authController.logout);
 authRouter.get("/logout-all", authController.logoutAll);
 
 
+authRouter.get("/verify-email", authController.verifyEmail);
+
+
 export default authRouter;
